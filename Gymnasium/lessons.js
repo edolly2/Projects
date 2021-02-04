8b5020645d97d197c6fc63d01a3750d0e1312547
edolly2 <edolly2@protonmail.com>
2021-02-04 02:50:22 -0600

    "Lesson 2"

--- Gymnasium/lessons.js
+++ Gymnasium/lessons.js
@@ -1,3 +1,26 @@
 document.getElementById("btn").addEventListener("click", function(event) {
     console.log("Hey, this works!");
 });
+
+
+var message;
+message = "Hello, World!";
+console.log(message);
+
+message = "Everything changes!";
+console.log(message);
+
+message = 101;
+console.log(message);
+
+message = true;
+console.log(message);
+
+var hello = "Hello, World!";
+console.log(hello);
+
+var firstName = "Eric",
+    lastName = "Dollinger",
+    age = 29;
+
+console.log(firstName, lastName, age);
